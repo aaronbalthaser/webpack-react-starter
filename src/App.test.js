a4b968a240ec03f0a8702849a98f656cbff2614c
Aaron Balthaser <aaron@Aarons-MacBook-Pro.local>
2020-01-05 20:04:20 -0800

    enabled jest.

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+
+import { App } from './App';
 
 test('renders without an error', () => {
-    
+  const wrapper = shallow(<App />);
 });
