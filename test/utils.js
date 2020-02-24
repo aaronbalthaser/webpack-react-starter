3bce382bb134b93406ea04adf78a7ddd5ca7f646
Aaron Balthaser <aaron@Aarons-MacBook-Pro.local>
2020-02-24 04:02:34 -0800

    fixed js doc warning and updated function signiture.

--- test/utils.js
+++ test/utils.js
@@ -2,18 +2,19 @@ import checkPropTypes from 'check-prop-types';
 
 /**
  * Returns shallow wrapper with the targeted node by data-test attribute.
- * @param {Wrapper} wrapper - Enzyme shallow wrapper to search within
- * @param {string} attr - Attr identifying data-test attribute
+ * @param {object} wrapper - Enzyme shallow wrapper to search within
+ * @param {string} value - Attr identifying data-test attribute
  */
 export const findByAttribute = (wrapper, value) => {
   return wrapper.find(`[data-test-id='${value}']`);
 };
 
-export const checkProp = (component, conformingProps) => {
+export const checkProp = (component, conformingProps, getStack = null) => {
   return checkPropTypes(
     component.propTypes,
     conformingProps,
     'prop',
-    component.name
+    component.name,
+    getStack
   );
 };
